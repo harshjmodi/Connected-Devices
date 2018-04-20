7c9d18aef9cc08570265eb80fa06119240b0bb73
Harsh Modi <harshmodi@manhattantradinggroup.com>
2018-04-19 21:50:24 -0400

    complete documentation

--- src/main/java/com/sdapuiot/SDAPIoT.java
+++ src/main/java/com/sdapuiot/SDAPIoT.java
@@ -30,19 +30,20 @@ public class SDAPIoT {
     }
 
     public void start() {
-        CoAPCommServer commServer = new CoAPCommServer("Harsh", "Bhautik", "Ronak");
-        CoAPCommClient coAPCommClient = new CoAPCommClient();
-        coAPCommClient.post("Harsh", "Dummy"); // use sensor.getData(); to get data and forward it
-        PressureSensorEmulator pressureSensor = (PressureSensorEmulator) SensorEmulatorFactory.getInstance().createSensorEmulator(SensorTypeEnum.PRESSURE);
-        GeolocationSensorEmulator geolocationSensor = (GeolocationSensorEmulator) SensorEmulatorFactory.getInstance().createSensorEmulator(SensorTypeEnum.GEOLOCATION);
+        CoAPCommServer commServer = new CoAPCommServer("Harsh", "Bhautik", "Ronak");// this will run on server in real application
+        CoAPCommClient coAPCommClient = new CoAPCommClient(); // this will run on sensor
+        coAPCommClient.post("Harsh", "Dummy"); // I had to send dummy data first because for some reason first message is going blank
+        //create different sensors to emulate different sensors
+        PressureSensorEmulator pressureSensor = (PressureSensorEmulator) SensorEmulatorFactory.getInstance().createSensorEmulator(SensorTypeEnum.PRESSURE,0,80);
+        GeolocationSensorEmulator geolocationSensor = (GeolocationSensorEmulator) SensorEmulatorFactory.getInstance().createSensorEmulator(SensorTypeEnum.GEOLOCATION, 10, 200);
         SpeedSensorEmulator speedSensor = (SpeedSensorEmulator) SensorEmulatorFactory.getInstance().createSensorEmulator(SensorTypeEnum.SPEED);
-        float pressure;
+        float pressure; // these variables can be made inline to send data directly
         String gps;
         String unit;
         float speed;
         String msg = "";
         for (int i = 0; i < 10; i++) {
-            speedSensor.run();
+            speedSensor.run();// run it every time to generate random values
             geolocationSensor.run();
             pressureSensor.run();
             pressure = speedSensor.getCurrentThreshold();
@@ -50,6 +51,7 @@ public class SDAPIoT {
             unit = "Pounds";
             speed = speedSensor.getCurrentThreshold();
             String group = "Automotive efficiency";
+            //embed the generated data into JSON format and send it to network
             msg = "\n{\n" +
                     " \"group\" : " + group + ",\n" +
                     " \"breakpressure\" : " + pressure + ",\n" +
@@ -58,14 +60,15 @@ public class SDAPIoT {
                     " \"unit\" : " + "\"" + unit + "\",\n" +
                     " \"time\" : " + "\"" + new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()) + "\"" + "\n" +
                     "}";
-            coAPCommClient.post("Harsh", msg); // use sensor.getData(); to get data and forward it
+            coAPCommClient.post("Harsh", msg);
             try {
+                //delay is must to make sure that data is completely traversed and doesn't exhaust sensor resources
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-//            coAPCommClient.post("Bhautik", "I'm good"); // use sensor.getData(); to get data and forward it
-//            coAPCommClient.post("Ronak", "Okay guys, see you then"); // use sensor.getData(); to get data and forward it
+//          coAPCommClient.post("Bhautik", "I'm good"); // use sensor.getData(); to get data and forward it
+//          coAPCommClient.post("Ronak", "Okay guys, see you then"); // use sensor.getData(); to get data and forward it
         }
         commServer.stop();
         _Logger.info("We're done here");
